613d21edabdb8aca1540d2487124e8e8ae0264f0
olixem <olixem@gmail.com>
2017-02-19 03:18:08 +0100

    Added vue-resource

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,12 @@
 var Vue = require("../bower_components/vue/dist/vue.js");
+var VueResource = require("../bower_components/vue-resource/dist/vue-resource.js");
+
+Vue.use(VueResource);
 
 new Vue({
     el: "#app",
     components:
     {
-        "hs-goals": require('./goalsComponent/main.js'),
+        "hs-goals": require('./goalsComponent/index.js'),
     }
 });
