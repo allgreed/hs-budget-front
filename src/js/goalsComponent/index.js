f9895c2684d227fca8575403211edb67544e44aa
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2020-10-10 22:48:43 +0200

    Make it work xD

--- src/js/goalsComponent/index.js
+++ src/js/goalsComponent/index.js
@@ -2,21 +2,43 @@ module.exports = {
     template: document.getElementById("template-goals").innerHTML,
     data: function()
     {
-        var link = 'http://allgreed.pythonanywhere.com/';
+        // TODO: fix the request xDDDD
+        //var link = 'http://allgreed.pythonanywhere.com/';
 
-        this.$http.get(link).then(
+        //this.$http.get(link).then(
 
-            function(response)
-            {
-                this.goals = response.body;
-            },
-            function(response)
-            {
-                console.log("Pszypau :c");
-            });
+            //function(response)
+            //{
+                //this.goals = response.body;
+            //},
+            //function(response)
+            //{
+                //console.log("Pszypau :c");
+            //});
 
         return {
-            goals: null,
+            goals: [
+                {
+                    currentMoney: 5,
+                    neededMoney: 100,
+                    text: "dziwki i kox",
+                },
+                {
+                    currentMoney: 500,
+                    neededMoney: 200,
+                    text: "rzutnixxx",
+                },
+                {
+                    currentMoney: 700,
+                    neededMoney: 1000,
+                    text: "czynż",
+                },
+                {
+                    currentMoney: 1,
+                    neededMoney: 5,
+                    text: "dłuuuuugi dłuuuugi tekst co przełamie linię i to tak w całości",
+                },
+            ],
         };
     },
     components:
